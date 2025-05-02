1cdc9cbc32a29d3ffb848b2d8a2c9d9746442901
shubkrsnha <sinhashubam542@gmail.com>
2025-05-02 13:19:51 +0530

    updating communication between backend and frontend

---
+++ frontend/emp-data-app/src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+/*export const environment = {
+    production: false,
+    apiUrl: 'http://127.0.0.1:5000/'
+  };
+  */
+  
+  export const environment = {
+    production: false,
+    apiUrl: 'http://flask-backend:5000/'   // Kubernetes service name
+  };
+  
--- frontend/emp-data-app/src/app/services/product.service.ts
+++ frontend/emp-data-app/src/app/services/product.service.ts
@@ -1,27 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 
-  export interface Product {
-    id: number;
-    name: string;
-    price: number;
-  }
-
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = environment.apiUrl;
 
-  private apiUrl = 'http://127.0.0.1:5000/';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl);
   }
 }
-
-
